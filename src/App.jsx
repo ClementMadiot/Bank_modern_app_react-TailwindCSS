580cc3b95bd5a5986e02405e9c8f4e80b8a53bcd
ClementMadiot <clementmadiot09@gmail.com>
2024-06-03 11:47:50 +0200

    fix tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import styles from './style'
 
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from './components'
+
 export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX}`}></div>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}><Navbar/></div>
 
       <div className={` bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}><Hero/></div>
       </div>
       <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Business Bulling Billing CardDeal Testimonials Clients CTA
-          Footer
+          <Stats/> 
+          <Business/> 
+          <Billing/> 
+          <CardDeal/> 
+          <Testimonials/> 
+          <Clients/> 
+          <CTA/>
+          <Footer/>
         </div>
       </div>
     </div>
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CTA from './CTA';
+import Clients from './Clients'
+import CardDeal from './CardDeal'
+import Stats from './Stats'
+import Footer from './Footer'
+import Testimonials from './Testimonials'
+import Hero from'./Hero'
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  CTA,
+  Clients,
+  CardDeal,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+}
